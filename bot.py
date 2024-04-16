9185d976f2bd0c55b3447c23989a0f65b39d0133
prabhsuratsingh <prabhsurat17@gmail.com>
2024-04-17 00:47:46 +0530

    Added response to message

--- bot.py
+++ bot.py
@@ -8,6 +8,14 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
+    async def on_message(self, message):
+
+        if message.author == self.user:
+            return
+        
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+
 intents = discord.Intents.default()
 intents.message_content = True
 
